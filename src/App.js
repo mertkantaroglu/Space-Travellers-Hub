dd1c04a062b5e8cc3310229dabb2add767706c79
Yemisirach <yemsrach3723@gmail.com>
2023-03-20 15:29:47 +0300

    Add my profile nav link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-const App = () => {
+import Rocket from "./components/Rocket";
+import Mission from "./components/Missions";
+import MyProfile from "./components/MyProfile";
+function App() {
   return (
-    <Navbar />
-  )
+    <BrowserRouter>
+      <div id="main-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Rocket />} />
+          <Route path="/Missions" element={<Mission />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import logo from "../img/planet.png"
-import "../styles/navbar.css"
+import { NavLink } from "react-router-dom";
+import logo from "../img/planet.png";
+import "../styles/navbar.css";
 
 function Navbar() {
   return (
@@ -9,6 +10,11 @@ function Navbar() {
           <img src={logo} className="logo" alt="website-logo" />
           <h1 className="home-title">Space Travelers' Hub</h1>
         </div>
+        <ul>
+          <li>
+            <NavLink to="/MyProfile">My Profile</NavLink>
+          </li>
+        </ul>
       </nav>
       <hr className="line" />
     </>
