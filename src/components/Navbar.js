a814cd3b7eb77a23f055ea6e34b5aaf3048204ef
Yemisirach <yemsrach3723@gmail.com>
2023-03-20 15:37:56 +0300

    Add my profile nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,25 @@ import logo from "../img/planet.png";
 import "../styles/navbar.css";
 
 function Navbar() {
+  const navbar = {
+    display: "inline-flex",
+    float: "right",
+    marginLeft: "5vw",
+  };
+  const linkListBox = {
+    listStyle: "none",
+    display: "flex",
+    fontSize: "1.2rem",
+  };
+  const linkStyle = {
+    textDecoration: "none",
+    color: "orange",
+    margin: "3vw",
+  };
+  const linkList = {
+    borderRight: "2px solid black",
+    borderLeft: "2px solid black",
+  };
   return (
     <>
       <nav className="nav">
@@ -10,9 +29,21 @@ function Navbar() {
           <img src={logo} className="logo" alt="website-logo" />
           <h1 className="home-title">Space Travelers' Hub</h1>
         </div>
-        <ul>
+        <ul style={linkListBox}>
           <li>
-            <NavLink to="/MyProfile">My Profile</NavLink>
+            <NavLink to="/" style={linkStyle}>
+              Rocket
+            </NavLink>
+          </li>
+          <li style={linkList}>
+            <NavLink to="/Missions" style={linkStyle}>
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/MyProfile" style={linkStyle}>
+              My Profile
+            </NavLink>
           </li>
         </ul>
       </nav>
