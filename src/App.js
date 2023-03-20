4c06e9b8e95306ef5f0013b01dafdc271597ffd2
Yemisirach <yemsrach3723@gmail.com>
2023-03-20 13:33:24 +0300

    Add nav link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./routes/Navbar";
+import Rocket from "./components/Rocket";
+import Mission from "./components/Missions";
+import MyProfile from "./components/MyProfile";
 function App() {
-  return (
-    <div className="App">
+  <BrowserRouter>
+    <div id="main-container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rocket />} />
+        <Route path="/Missions" element={<Mission />} />
+        <Route path="/MyProfile" element={<MyProfile />} />
+      </Routes>
     </div>
-  );
+  </BrowserRouter>;
 }
 
 export default App;
