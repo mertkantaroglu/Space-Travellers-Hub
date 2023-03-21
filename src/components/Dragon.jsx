fc28c2a82fd40ee4d98f4431d14cf465a084dc4f
unknown <sohailshahidi1435@gmail.com>
2023-03-21 19:48:21 +0430

    Display Dragons

--- src/components/Dragon.jsx
+++ src/components/Dragon.jsx
@@ -1,5 +1,44 @@
-const Dragon = () => {
-  return <div>Dragon</div>;
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDragon } from '../redux/dragon/dragonSlice'
+
+function Dragons({
+  name, type, image,
+}) {
+  return (
+
+    <div>
+      <h1>{name}</h1>
+      <p>{type}</p>
+      <img src={image} alt="dragon" />
+    </div>
+  );
+}
+
+Dragons.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
+
+function Dragon() {
+  const dragonsArr = useSelector((state) => state.dragons.dragonStore);
+  const dispatch = useDispatch();
+  useEffect(() => {dispatch(getDragon());}, [dispatch]);
+  return (
+    <div>
+      {dragonsArr.map((dragons) => (
+        <Dragons
+          key={dragons.id}
+          id={dragons.id}
+          name={dragons.name}
+          type={dragons.type}
+          image={dragons.flickr_images}
+        />
+
+      ))}
+    </div>
+  );
+}
 
 export default Dragon;
