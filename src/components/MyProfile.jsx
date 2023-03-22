289a8d5bda0db2917fa22fd56e1789f7d44beba1
Yemisirach <yemsrach3723@gmail.com>
2023-03-23 01:15:45 +0300

    Add rocket list inside myprofile

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,19 +1,39 @@
 import MissionProfile from "./Mission/MissionProfile";
-import { ReservedDragons } from './Dragon';
+import { ReservedDragons } from "./Dragon";
+import { Table, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSelector } from "react-redux";
 
 const MyProfile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
   return (
-  <>
-    <div className="mission-profile">
-      <MissionProfile />
-    </div>
-    <div className="my_dragons">
-      <h2>My Dragons</h2>
-      <ul className="dragons_list">
-        <ReservedDragons />
-      </ul>
-    </div>
-  </>
+    <>
+      <Row className="m-5">
+        <Col sm={6}>
+          <div className="mission-profile">
+            <MissionProfile />
+          </div>
+          <div className="my_dragons">
+            <h2>My Dragons</h2>
+            <ul className="dragons_list">
+              <ReservedDragons />
+            </ul>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <h2>My Rockets</h2>
+          <Table hover variant="light">
+            <tbody>
+              {rockets.map((rocket) => (
+                <tr key={rocket.id}>
+                  <td className="p-4">{rocket.rocket_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
   );
 };
 
